d1576fc5277422e1fc31663ebc71c4fb725c382b
jaya <jayaseyyadri@gmail.com>
2021-01-21 23:40:17 -0600

    first commit

---
+++ src/main/java/com/sample/SelectLiquorServlet.java
@@ -0,0 +1,9 @@
+package com.sample;
+import com.sample.model.LiquorType;
+import sun.net.www.http.HttpClient;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet()
+public class SelectLiquorServlet extends HttpClient {
+}
